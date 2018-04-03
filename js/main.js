fd4be628d5161f3f3f9d61bea014f1820a0080a0
Enrico Mengotti <enrico.mengotti@gmail.com>
2018-04-03 17:02:34 +0200

    fix: small viewport size style of index.html
    style the mobile fisrt aspect of index.html define the card style for
    the rstaurant list. Set first breackpoint at min-width 500px

--- js/main.js
+++ js/main.js
@@ -137,30 +137,38 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  const div = document.createElement('div');
+  div.classList.add("card");
+  li.append(div);
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  div.append(image);
+
+  const divContainer = document.createElement('div');
+  divContainer.classList.add('card-container');
+  div.append(divContainer);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  divContainer.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  divContainer.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  divContainer.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  divContainer.append(more);
 
-  return li
+
+  return li;
 }
 
 /**
